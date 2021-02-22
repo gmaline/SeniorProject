44a8af46b055cfa1915b4ef08d10bbdb3a86b866
gmaline <66379163+gmaline@users.noreply.github.com>
2021-02-21 18:47:52 -0600

    In this commit, the two extra genes on either end of the BCT&BK record that i was using. This commit also contains the two final enzymes of the BK pathway and the final enzyme of the BCT pathway which was not included in the first record.

--- formatPathwayGenes.py
+++ formatPathwayGenes.py
@@ -1,26 +1,35 @@
 from Bio import SeqIO
 
-def run():
-    gene_locations1 = {'Fe-S oxidoreductase': [0, 1245],
-                      'thiolase': [1462, 2647],
-                      'crotonase': [2696, 3488],
-                      'beta hydroxybutyryl-CoA dehydrogenase': [3639, 4512],
-                      'butyryl-CoA dehydrogenase': [4548, 5715],
-                      'electron transfer flavoprotein beta-subunit': [5731, 6517],
-                      'electron transfer flavoprotein alpha-subunit': [6595, 7606],
-                      'putative multidrug efflux pump': [7978, 8390]}
 
-    gene_locations2 = {'Fe-S oxidoreductase': [0, 1245],
-                        ''
-    for record in SeqIO.parse("butyrate_genes_1.fasta", "fasta"):
+def run():
+    gene_locations_BTCnBK = {"thiolase": [1462, 2647],
+                             "crotonase": [2696, 3488],
+                             "beta hydroxybutyryl-CoA dehydrogenase": [3639, 4512],
+                             "butyryl-CoA dehydrogenase": [4548, 5715],
+                             "electron transfer flavoprotein beta-subunit": [5731, 6517],
+                             "electron transfer flavoprotein alpha-subunit": [6595, 7606]}
+
+    for record in SeqIO.parse("butyrate_genes_BTC&BK.fasta", "fasta"):
         sequence = record.seq
 
-    gene_sequences = {} #Will contain the gene sequences broken up by their start and end positions
+    gene_sequences = {}  # Will contain the gene sequences broken up by their start and end positions
 
-    #Iterate through the locations dictionary and create entries for each gene with their
+    # Iterate through the locations dictionary and create entries for each gene with their
     # full sequences.
-    for key in gene_locations1.keys():
-        gene_sequences[key] = sequence[gene_locations1[key][0]:gene_locations1[key][1]]
+    for key in gene_locations_BTCnBK.keys():
+        gene_sequences[key + ", BCT&BK"] = sequence[gene_locations_BTCnBK[key][0]:gene_locations_BTCnBK[key][1]]
+
+    for record in SeqIO.parse("butyrate_kinase_BK.fasta", "fasta"):
+        sequence = record.seq
+        gene_sequences["butyrate kinase, BK"] = sequence
+
+    for record in SeqIO.parse("phosphate_butyryltransferase_BK.fasta", "fasta"):
+        sequence = record.seq
+        gene_sequences["phoasephate butyryltransferase, BK"] = sequence
+
+    for record in SeqIO.parse("butyryl_COA_transferase_BCT.fasta", "fasta"):
+        sequence = record.seq
+        gene_sequences["butyryl-COA-transferase, BCT"] = sequence[392:1733]
 
     outfile = open("butyrate_genes_sep1.fasta", 'a')
     for key in gene_sequences:
